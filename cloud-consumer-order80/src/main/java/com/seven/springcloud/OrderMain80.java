dbc7ac3a288efe68322b345ac46317bec2ebc4d5
Seven Wong <seven@huiaj.com>
2021-01-08 12:16:15 +0800

    新增：2个项目注册进eureka

--- cloud-consumer-order80/src/main/java/com/seven/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/seven/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.seven.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author SevenWong
  * @date 2021-01-07 17:41:25
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
